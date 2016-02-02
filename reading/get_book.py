e1de71282d007a86d76d13537d4796d44f4724ba
Maonx <imaonx@gmail.com>
2016-02-02 10:59:57 +0800

    修改章节过滤正则；添加《我欲封天》
    修改脚本名称；修改正则

--- reading/get_book.py
+++ reading/get_book.py
@@ -20,12 +20,16 @@ def ungzip(data):
     except:
         pass
 
-def del_same(chapters):
+def filter_chapter(chapters):
     seen = set()
+    patten = re.compile(r'楔子|外传|章|卷|节');
     for chapter in chapters:
-        if chapter[2] not in seen:
+        chapter_name = chapter[1]
+        if not patten.search(chapter_name):
+            continue
+        if chapter_name not in seen:
             yield chapter
-            seen.add(chapter[2])
+            seen.add(chapter_name)
 
 def download_webpage(url):
     header = {
@@ -55,8 +59,7 @@ def get_chapter_content(chapter):
     chapter_content = download_webpage(chapter_url)
     chapter_content = regex.search(chapter_content).group(1)
     chapter_content = re.sub(r'<br.?/>','\n',chapter_content)
-    chapter_content = re.sub(r'&nbsp;','',chapter_content)
-    chapter_content = re.sub(r'\r\n','',chapter_content)
+    chapter_content = re.sub(r'&nbsp;|\r\n','',chapter_content)
     return (title,chapter_content)
 
 def download_book(book):
@@ -78,7 +81,6 @@ def generate_post(bookname, title, content):
     post_content =  title + date + tag + '---\n' + content
     with open(post_name, 'wt') as f:
         f.write(post_content)
-    print('已生成: ' + post_name )
 
 def get_catalog_content(book):
     global SITE
@@ -90,10 +92,10 @@ def get_catalog_content(book):
     return catalog
 
 def get_chapter_list(catalog):
-    regex = re.compile(r'<li><a href="(.*)">(.*(第[零一二三四五六七八九十百千]*[章|节]).*)</a></li>')
+    regex = re.compile(r'<li><a href="(.*)">(.*)</a></li>')
     try:
         chapters = regex.findall(catalog)
-        return list(del_same(chapters))
+        return list(filter_chapter(chapters))
     except:
         print('Catalog format error')
 
@@ -122,73 +124,12 @@ def get_latest_posts(book):
     catalog = get_catalog_content(book)
     chapters = get_chapter_list(catalog)
     print(book['bookname'] + '有 ' + str(len(chapters)) + ' 个章节更新')
-    for chapter in chapters:
+    for index, chapter in enumerate(chapters):
         title, content = get_chapter_content(chapter)
         generate_post(book['bookname'], title, content)
+        print('已生成(%2d/%d): %s' % (index+1, len(chapters), title))
         book['latest'] = chapter[0]
 
-
-
-def update(book):
-    # global path
-    url = book['read_url']
-    # latest_chapter_url = book['latest_chapter_url']
-    catalog = download_webpage(url)
-    if book['latest_chapter_url'] :
-        try:
-            catalog = re.search(r'(?<=href="'+book['latest_chapter_url']+').*',
-                            catalog, re.S).group(0)
-        except:
-            print("catalog format error,get failed")
-            return 0
-    have_new_chapter = parse_chapters(catalog)
-    if have_new_chapter:
-        for i in have_new_chapter:
-            title = ' '.join([book['book_name'], i[1].strip()])
-            try_time = 0
-            while True:
-                try:
-                    chapter_content = parse_chapter_content(i)
-                    # chapter_content = re.sub(r'.{,3}燃.{,3}文.{,3}小.{,3}[说說].?', "", chapter_content)
-                    # chapter_content = re.sub(r'[wW]{,3}\.[a-zA-Z]{6}\.[a-zA-Z]{3}.?', "", chapter_content)
-                    chapter_content = re.sub(r'&nbsp;','', chapter_content)
-                    break
-                except:
-                    try_time += 1
-                    if try_time > 19:
-                        break
-            if try_time == 20:
-                # print("Update failed: " + title)
-                failed_list.append(title)
-                continue
-            chapter_content = md_format(title, chapter_content,
-                                        book['category'], book['book_name'])
-            save_path = path + title + '.md'
-            save_chapter(save_path, chapter_content)
-        book['latest_chapter_url'] = have_new_chapter[-1][0]
-        return 1
-
-# def download_failed_list(chapter, category, book_name):
-#     global update_time
-#     update_time = update_time+timedelta(seconds=1)
-#     title='title: '+title+'\n'
-#     date = 'date: '+update_time.strftime("%Y-%m-%d %H:%M:%S")+'\n'
-#     chapter_content = parse_chapter_content(chapter[0])
-
-
-# if __name__ == "__main__" :
-#     # path = r'/home/vagrant/update-latest-novel/source/_posts/'
-#     failed_list = []
-#     is_update=0
-#     books = get_books("books.json")
-#     for book in books:
-#         if update(book):
-#             save_books(books, "books.json")
-#     if failed_list :
-#         print("Update faild %d chapters!!" % len(failed_list))
-#         print('\n'.join(failed_list))
-#         with open('Failed.log','w') as fp:
-#             fp.write('\n'.join(failed_list))
 
 if __name__ == "__main__" :
     books = get_books("books.json")
